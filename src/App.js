9f3ebee5201dd176066d2ce359dda9c2cd9c3ef8
Anjan Rai <rai.unknown@gmail.com>
2020-02-11 18:39:17 +1100

    mobile first design for login and signup forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUpForm from "./components/SignUpForm";
+import {Tabs} from 'react-bootstrap';
+import Tab from "react-bootstrap/Tab";
+import LoginForm from "./components/LoginForm";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "./components/common/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    return(
+        <div>
+            <Header/>
+            <Container>
+                <Row>
+                    <Col sm={8} md={7} lg={8}>
+                        Description
+                    </Col>
+                    <Col sm={4} md={5} lg={4}>
+                        <Tabs defaultActiveKey="signup" id="uncontrolled-tab-example">
+                            <Tab eventKey="signup" title="Sign Up">
+                                <SignUpForm/>
+                            </Tab>
+                            <Tab eventKey="login" title="Login">
+                                <LoginForm/>
+                            </Tab>
+                        </Tabs>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+  }
 }
 
 export default App;
